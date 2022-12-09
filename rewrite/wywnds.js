1e562d3c4b158ebd239094d55b18818266d7a89a
3488888725 <115656000+3488888725@users.noreply.github.com>
2022-12-09 23:20:46 +0800

    Update wywnds.js

--- rewrite/wywnds.js
+++ rewrite/wywnds.js
@@ -1,33 +1,13 @@
+/******
+作者：林阿斗
+******/
+
 [rewrite_local]
 ^https?:\/\/p\.du\.163\.com\/gain\/readtime\/info\.json*? url script-response-body https://raw.githubusercontent.com/3488888725/Quantumult-X/main/rewrite/wywnds.js
-
 
 [mitm]
 hostname = p.du.163.com
 
-var Url = $request.url;
-function setQueryString(key, val) { 
-    var url = Url.split('?'), search=url[1];
-    var query = {};
-    if (search) {
-        search.split('&').forEach((item) => {
-            var arr = item.split('=');
-            query[arr[0]] = arr[1];
-        });
-    }
-    query[key] = val;
-    var queryArr = [];
-    for (var p in query) {
-        queryArr.push(p + '=' + query[p]);
-    }
-    return url[0]+'?'+queryArr.join('&');
-};
-
-
-console.log(Url);
-$done({ url:Url });
-
-//
 var body=$response.body;
 body = body.replace(/"tradeEndTime\":\d+/g,'"tradeEndTime":4102373543494');
 $done(body);
